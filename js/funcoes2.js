e6cfc9bebb21e59d1037ba7c8153bf86d331b89f
Danilo Arantes <danilo@arantes>
2016-09-25 11:54:10 -0300

    Arquivos para calculo de horas

---
+++ js/funcoes2.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+
+	camposMarcados = new Array();
+	var datainicio, datafim;
+	pegaData();
+
+	$("#bt").click(function(){
+
+		verificaCheckBox();
+		calculaDatas();
+
+		//alert("Inicio: " + datainicio.toLocaleDateString() + " Fim: " + datafim.toLocaleDateString());
+	});
+
+
+function pegaData(){
+	$("#inicio").datepicker();
+	$("#fim").datepicker();
+
+	$("#inicio").change( function() {
+	datainicio = $(this).datepicker("getDate");
+	});
+
+	$("#fim").change( function() {
+	datafim = $(this).datepicker("getDate");
+	});
+};
+
+function calculaDatas(){
+	var dInicio = new Date(datainicio);
+	var dFim = new Date(datafim);
+	var scheduler = new Array();
+	var inicio = dInicio.getDate();
+
+
+	var i = 0;
+	var f = new Date();
+	f.setDate(inicio + 1);
+	
+//console.log(dInicio);
+
+	while (i < 5) {
+
+		f.setDate(f.getDate() + 1);
+		scheduler.push(f.toDateString());
+	//	console.log(f);
+
+		i++;
+	};
+
+
+
+console.log(scheduler.valueOf());
+
+
+
+
+
+
+
+
+};
+
+function verificaCheckBox(){
+
+	$("input[type=checkbox][name='semana[]']:checked").each(function(){
+		var vLen, i;
+		var text = "";
+		camposMarcados.push($(this).val());
+		vLen = camposMarcados.length;
+
+		for (i = 0; i < vLen; i++){
+			text += "<p>" + camposMarcados[i] + "</p>";
+		}
+
+		document.getElementById("result").innerHTML = text;
+	});
+};
+
+
+function formataData() {
+	$('#inicio').datepicker( "option", "dateFormat", "dd-mm-yy" ).val();
+	$('#fim').datepicker( "option", "dateFormat", "dd-mm-yy" ).val();
+};
+
+
+
+
+
+});
